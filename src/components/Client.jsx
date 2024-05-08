b1053bbc9e22c5d51331e4dd453b351a54b42b17
TimiMaliki <malikitimi@gmail.com>
2024-05-08 17:24:16 +0100

    mobile responsiveness

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -23,7 +23,7 @@ const Client = () => {
         </p>
       </div>
 
-      <Atropos className="my-atropos" shadow={false}>
+      <Atropos className="my-atropos hidden lg:block" shadow={false}>
       <div className="p-14 lg:p-12 flex justify-center">
       <img src={car} alt="dot" />
       </div>
