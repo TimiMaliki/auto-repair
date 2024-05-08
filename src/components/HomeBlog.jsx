654c1cb47d13fcbe48b0ea5cbdd05eb17d77be9d
TimiMaliki <malikitimi@gmail.com>
2024-05-08 17:57:20 +0100

    services

--- src/components/HomeBlog.jsx
+++ src/components/HomeBlog.jsx
@@ -8,7 +8,7 @@ const HomeBlog = () => {
     <div className="w-full h-full mt-12">
       <div className="w-full h-full flex justify-between gap-8 p-8 lg:p-14">
         <div className="our-services">
-        <Link to={"uitil"}><h1 className="text-5xl  lg:text-7xl font-extrabold">3D uitlijnen</h1></Link>  
+        <Link to={"uitil"}><h1 className="text-3xl  lg:text-7xl font-extrabold">3D uitlijnen</h1></Link>  
         </div>
 
         <div className="text w-1/2 flex justify-end gap-10 mt-1">
