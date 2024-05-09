4415e89adfb44d0f0fc0eb439a4a4e893ee379f6
TimiMaliki <malikitimi@gmail.com>
2024-05-09 09:39:54 +0100

    added:scroll top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "../src/Router/Router";
+import ScrollToTop from "../src/components/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
+      <ScrollToTop />
     </BrowserRouter>
   );
 }
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router";
 import MainLayout from "../Layouts/MainLayout.jsx";
 import Uitilinen from "../page/Uitilinen.jsx";
 import  Diagnose from "../page/ Diagnose.jsx"
-import ScrollToTop from "../components/ScrollToTop.jsx";
 import App from "../components/App.jsx";
 
 
@@ -12,7 +11,6 @@ import App from "../components/App.jsx";
 const Router = () => {
   return (
     <Routes>
-      <ScrollToTop/>
       <Route path="" element={<MainLayout />}>
         <Route index element={<App />} />
         <Route path="/uitil" element={<Uitilinen/>}/>
