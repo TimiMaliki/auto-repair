c7fb80b1dabb167d5ef0dbb9f7975ea6d8810488
TimiMaliki <malikitimi@gmail.com>
2024-05-09 09:28:54 +0100

    added: reduced the size of text

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import MainLayout from "../Layouts/MainLayout.jsx";
 import Uitilinen from "../page/Uitilinen.jsx";
 import  Diagnose from "../page/ Diagnose.jsx"
+import ScrollToTop from "../components/ScrollToTop.jsx";
 import App from "../components/App.jsx";
 
 
@@ -11,6 +12,7 @@ import App from "../components/App.jsx";
 const Router = () => {
   return (
     <Routes>
+      <ScrollToTop/>
       <Route path="" element={<MainLayout />}>
         <Route index element={<App />} />
         <Route path="/uitil" element={<Uitilinen/>}/>
